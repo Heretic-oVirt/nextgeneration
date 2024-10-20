7fa88c7307baada2c5af3d94ee351878c61a212a
Giuseppe Ragusa <giuseppe.ragusa@hotmail.com>
2024-10-20 11:09:19 +0200

    Initial commit of base hvp_parameters.sh

---
+++ variants/base/hvp_parameters.sh
@@ -0,0 +1,17 @@
+# Set default parameters
+
+# Define pre and post hook functions
+
+function pre_install_hook_custom_actions() {
+	echo "No pre-install custom actions." 2>&1
+}
+
+function post_install_hook_custom_actions() {
+	echo "No post-install custom actions." 2>&1
+	# Extract kernel commandline parameters
+
+	# Define settings
+
+	# Generate dynamic configuration files
+
+}
